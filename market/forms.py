87b154402f18f7f93a5813612a4ad9979fc8c8fd
frhn16 <faroomirz@hotmail.com>
2025-02-20 00:01:32 +0000

    Added validations

--- market/forms.py
+++ market/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, EqualTo, Email, DataRequired # Adds validation
 
 class RegisterForm(FlaskForm):
-    username = StringField(label="Username:") # Label is what is displayed on webpage
-    email_address = StringField(label="Email:")
-    password1 = PasswordField(label="Password 1:")
-    password2 = PasswordField(label="Password 2:")
+    username = StringField(label="Username:", validators=[Length(min=2, max=30), DataRequired()]) # Label is what is displayed on webpage
+    email_address = StringField(label="Email:", validators=[Email(), DataRequired()])
+    password1 = PasswordField(label="Password 1:", validators=[Length(min=6, max=30), DataRequired()])
+    password2 = PasswordField(label="Password 2:", validators=[EqualTo("password1"), DataRequired()])
     submit = SubmitField(label="Create Account")
--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,8 @@
 from market import app
-from flask import render_template
-from market.models import Item
+from flask import render_template, redirect, url_for
+from market.models import Item, User
 from market.forms import RegisterForm
+from market import db
 
 @app.route("/") # Decorator for webpage URL
 @app.route("/home")
@@ -9,7 +10,7 @@ def home_page():
     return render_template("home.html")
 
 @app.route("/market")
-def market():
+def market_page():
     items = Item.query.all() # Items includes all entered items
     return render_template("market.html", items=items)
 
@@ -17,7 +18,17 @@ def market():
 def about_page(username):
     return f"<h1>About Page for {username}<h1>"
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,
+                              email_address=form.email_address.data,
+                              password_hash=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for("market_page"))
+    if form.errors != {}: # If no validation errors
+        for err_message in form.errors.values():
+            print(f"There was an error with the user {err_message}")
     return render_template("register.html", form=form)
